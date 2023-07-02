317012ac022ba2b674efb905fac659f28c21f500
bagasadtm <bagasadt1922@gmail.com>
2023-07-02 13:47:26 +0700

    tugas-5

---
+++ tugas-5/latihan_2/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+/*
+    TUGAS ALPRO 5 - STRUCT & POINTER
+
+    Nama : Bagas Aditama H
+    NIM : A11.2022.14322
+*/
+
+using namespace std;
+
+struct Sepeda {
+    string merk;
+    string type;
+    int tahun;
+    string harga;
+};
+
+int main() {
+    Sepeda sepedaGunung;
+    sepedaGunung.merk = "Polygon";
+    sepedaGunung.type = "Sepeda Gunung";
+    sepedaGunung.tahun = 2013;
+    sepedaGunung.harga = "2.000.000";
+
+    cout << "Merk: " << sepedaGunung.merk << endl;
+    cout << "Type: " << sepedaGunung.type << endl;
+    cout << "Tahun: " << sepedaGunung.tahun << endl;
+    cout << "Harga: " << sepedaGunung.harga << endl;
+
+    return 0;
+}
